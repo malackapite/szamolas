01d7155dd24d03fc13d90e0d113fa0b8c5f7079f
malackapite <veress.marcell@protonmail.com>
2023-03-03 12:35:22 +0100

    ciklusok

---
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -0,0 +1,12 @@
+
+package ciklusok;
+
+
+public class Szamok {
+    public static void main(String[] args) {
+        for(int ix=2;ix<=4;ix++)
+            for(int jx=2;jx<=4;jx++)
+                for(int kx=2;kx<=4;kx++)
+                    System.out.printf("%d%d%d\r\n",ix,jx,kx);
+    }
+}
